86cfd62723611fee718c14450afe25acea5f4acb
Nathaniel Stambaugh <macdaddy101@icloud.com>
2022-06-14 14:46:20 -0400

    Redesigned

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,89 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtTKW = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtKW = new System.Windows.Forms.TextBox();
-            this.txtSQFT = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.txtDoit = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Enter KW from the ELE file:";
-            // 
-            // txtTKW
-            // 
-            this.txtTKW.Location = new System.Drawing.Point(154, 17);
-            this.txtTKW.Name = "txtTKW";
-            this.txtTKW.Size = new System.Drawing.Size(100, 20);
-            this.txtTKW.TabIndex = 1;
-            this.txtTKW.TextChanged += new System.EventHandler(this.txtTKW_TextChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 60);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Calculated KiloWatts:";
-            // 
-            // txtKW
-            // 
-            this.txtKW.Location = new System.Drawing.Point(154, 53);
-            this.txtKW.Name = "txtKW";
-            this.txtKW.ReadOnly = true;
-            this.txtKW.Size = new System.Drawing.Size(100, 20);
-            this.txtKW.TabIndex = 3;
-            this.txtKW.TextChanged += new System.EventHandler(this.txtKW_TextChanged);
-            // 
-            // txtSQFT
-            // 
-            this.txtSQFT.Location = new System.Drawing.Point(154, 94);
-            this.txtSQFT.Name = "txtSQFT";
-            this.txtSQFT.ReadOnly = true;
-            this.txtSQFT.Size = new System.Drawing.Size(100, 20);
-            this.txtSQFT.TabIndex = 4;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 101);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(121, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Calculated Square Feet:";
             // 
             // txtDoit
             // 
-            this.txtDoit.Location = new System.Drawing.Point(95, 137);
+            this.txtDoit.Location = new System.Drawing.Point(88, 173);
             this.txtDoit.Name = "txtDoit";
             this.txtDoit.Size = new System.Drawing.Size(75, 23);
             this.txtDoit.TabIndex = 6;
-            this.txtDoit.Text = "Do the thing";
+            this.txtDoit.Text = "Git \'er done";
             this.txtDoit.UseVisualStyleBackColor = true;
             this.txtDoit.Click += new System.EventHandler(this.txtDoit_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(15, 12);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(371, 155);
+            this.txtInput.TabIndex = 7;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.AcceptsReturn = true;
+            this.txtOutput.Location = new System.Drawing.Point(15, 202);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(371, 192);
+            this.txtOutput.TabIndex = 8;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Enabled = false;
+            this.btnClear.Location = new System.Drawing.Point(198, 173);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(89, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear dat thing";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(274, 172);
+            this.ClientSize = new System.Drawing.Size(399, 408);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
             this.Controls.Add(this.txtDoit);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtSQFT);
-            this.Controls.Add(this.txtKW);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtTKW);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Space Plan Sizer";
             this.ResumeLayout(false);
@@ -119,14 +92,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtTKW;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtKW;
-        private System.Windows.Forms.TextBox txtSQFT;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button txtDoit;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -24,32 +24,108 @@ namespace Space_Plan_Sizer__.NET_4._8_
 
         private void txtDoit_Click(object sender, EventArgs e)
         {
-            if (String.IsNullOrEmpty(txtTKW.Text))
+            if (String.IsNullOrEmpty(txtInput.Text))
             {
-                MessageBox.Show("Please enter the KiloWatts listed in the ELE file");
-                txtTKW.Focus();
-            } else
-            {
-                decimal tkw = Convert.ToDecimal(txtTKW.Text);
-                decimal kw = tkw / 2;
-                decimal sqft = kw * 50 / 3;
-                txtKW.Text = kw.ToString();
-                txtSQFT.Text = Math.Round(sqft,1).ToString();
+                MessageBox.Show("Please paste the KiloWatts and Quantity listed in the ELE file");
+                txtInput.Focus();
             }
-            
+            else
+            {
+                if (!txtInput.Text.Contains("Quantity") && !txtInput.Text.Contains("KiloWatt"))
+                {
+                    MessageBox.Show("Column headers are missing. Please paste the Quantity and KiloWatts field, including the headers, into the textbox");
+                    txtInput.Focus();
+                }
+                else
+                {
+                    int index1 = txtInput.Text.IndexOf("Q");
+                    int index2 = txtInput.Text.IndexOf("y");
+                    int index3 = txtInput.Text.IndexOf("K");
+
+                    if (index1 != 0 && index2 != 9)
+                    {
+                        MessageBox.Show("Please make sure that the column headers are in the right order: Quantity first and then KiloWatt second");
+                        txtInput.Focus();
+                    }
+                    else
+                    {
+                        txtInput.Text = txtInput.Text.TrimEnd();
+                        int ikw = index3 + 10;
+                        string q = txtInput.Text.Substring(index2 + 3, index3 - index2 - 5);
+                        string k = txtInput.Text.Substring(ikw, txtInput.Text.Length - ikw);
+                        string[] q1 = q.Split('\r');
+                        string[] k1 = k.Split('\r');
+                        int[] iq1 = new int[q1.Length];
+                        decimal[] dk1 = new decimal[k1.Length];
+                        decimal tkw = 0;
+
+                        // Trim the whitespace from array elements and convert to int32
+                        for (int i = 0; i < q1.Length; i++)
+                        {
+                            q1[i] = q1[i].Trim();
+                            if (q1[i] != "")
+                            {
+                                iq1[i] = Convert.ToInt32(q1[i]);
+                            }
+                        }
+
+                        // Trim the whitespace from array elements and convert to decimal
+                        for (int i = 0; i < k1.Length; i++)
+                        {
+                            k1[i] = k1[i].Trim();
+                            if (k1[i] != "")
+                            {
+                                dk1[i] = Convert.ToDecimal(k1[i]);
+                            }
+                        }
+
+                        // Calculate total KW
+                        for (int i = 0; i < iq1.Length; i++)
+                        {
+                            tkw += iq1[i] * dk1[i];
+                        }
+
+                        // Calculate using site formula
+                        decimal kw = tkw / 2;
+                        decimal sqft = kw * 50 / 3;
+
+                        txtOutput.Text = "Sized at " + Convert.ToString(kw) + " kw and " + Convert.ToString(Math.Round(sqft, 1)) + " sqft.";
+
+
+
+
+                    }
+                }
+
+
+
+               
+
+
+            }
+
+        
+                    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+            txtOutput.Text = "";
         }
 
-        private void txtTKW_TextChanged(object sender, EventArgs e)
+        private void txtInput_TextChanged(object sender, EventArgs e)
         {
-            if (txtTKW.Text.Trim() == "") return;
-            for (int i = 0; i < txtTKW.Text.Length; i++)
+            if (txtInput.Text != "")
             {
-                if (!char.IsDigit(txtTKW.Text[i]) && txtTKW.Text[i] != '.')
-                {
-                    MessageBox.Show("Please enter a valid number");
-                    txtTKW.Clear();
-                }
+                btnClear.Enabled = true;
+            }
+            else
+            {
+                btnClear.Enabled = false;
             }
         }
     }
 }
+        
+    
+
